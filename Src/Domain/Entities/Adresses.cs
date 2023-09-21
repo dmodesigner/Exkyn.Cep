139529fdc934dc89a12f41f75a401fe40de9238c
Daniel Moura <daniel.dmo@live.com>
2023-09-20 22:41:30 -0300

    Adicionado as entidades do projeto.

---
+++ Src/Domain/Entities/Adresses.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Adresses
+    {
+        public int AddressID { get; set; }
+        public int NeighborhoodID { get; set; }
+        public int CityID { get; set; }
+        public int StateID { get; set; }
+        public string? ZipCode { get; set; }
+        public string? Address { get; set; }
+        public virtual Neighborhoods? Neighborhoods { get; set; }
+        public virtual Cities? Cities { get; set; }
+        public virtual States? States { get; set; }
+    }
+}
---
+++ Src/Domain/Entities/Cities.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class Cities
+    {
+        public int CityID { get; set; }
+        public int StateID { get; set; }
+        public string? ZipCode { get; set; }
+        public string? City { get; set; }
+        public bool Capital { get; set; }
+        public virtual States? States { get; set; }
+    }
+}
