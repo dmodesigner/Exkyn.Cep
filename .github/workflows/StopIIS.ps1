542f2ebf2b0ad2f6819f82cb012655387b505e1e
Daniel Moura <daniel.dmo@live.com>
2024-06-04 18:17:17 -0300

    Update StopIIS.ps1

--- .github/workflows/StopIIS.ps1
+++ .github/workflows/StopIIS.ps1
@@ -4,9 +4,6 @@ Import-Module WebAdministration
 # Definir o nome do pool de aplicativos como parâmetro
 $appPoolName = $args[0]
 
-# Definir a ação (opcional)
-$action = "Stop"
-
 # Obter o objeto do pool de aplicativos
 $appPool = Get-WebSite -Name "Default" | Get-WebSiteApplicationPool -Name $appPoolName
 
