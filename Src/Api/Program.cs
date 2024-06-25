56550cadbb663b40d5d6df279348613e0029e794
Daniel Moura <daniel.dmo@live.com>
2024-06-24 22:21:44 -0300

    Otimização de codigos: Simplificação da resposta da API.

--- Src/Api/Program.cs
+++ Src/Api/Program.cs
@@ -51,37 +51,19 @@ else
 
 #region Endpoint da API
 
-app.MapGet("/estado/", ([FromServices] IStateService stateService) => Results.Ok(new ReturnModel<States>
-{
-    List = stateService.SearchByState()
-}));
+app.MapGet("/estado/", ([FromServices] IStateService stateService) => Results.Ok(stateService.SearchByState()));
 
-app.MapGet("/cidade/{estadoID}", ([FromServices] ICityService cityService, int estadoID) => Results.Ok(new ReturnModel<Cities>
-{
-    List = cityService.SearchByState(estadoID)
-}));
+app.MapGet("/cidade/{estadoID}", ([FromServices] ICityService cityService, int estadoID) => Results.Ok(cityService.SearchByState(estadoID)));
 
-app.MapGet("/cidade/buscar/{uf}", ([FromServices] ICityService cityService, string uf) => Results.Ok(new ReturnModel<Cities>
-{
-    List = cityService.SearchByState(uf)
-}));
+app.MapGet("/cidade/buscar/{uf}", ([FromServices] ICityService cityService, string uf) => Results.Ok(cityService.SearchByState(uf)));
 
-app.MapGet("/bairro/{estadoID}/{cidadeID}", ([FromServices] INeighborhoodService neighborhoodService, int estadoID, int cidadeID) => Results.Ok(new ReturnModel<Neighborhoods>
-{
-    List = neighborhoodService.SearchByStateAndCity(estadoID, cidadeID)
-}));
+app.MapGet("/bairro/{estadoID}/{cidadeID}", ([FromServices] INeighborhoodService neighborhoodService, int estadoID, int cidadeID) => Results.Ok(neighborhoodService.SearchByStateAndCity(estadoID, cidadeID)));
 
-app.MapGet("/bairro/buscar/{uf}/{cidade}", ([FromServices] INeighborhoodService neighborhoodService, string uf, string cidade) => Results.Ok(new ReturnModel<Neighborhoods>
-{
-    List = neighborhoodService.SearchByStateAndCity(uf, cidade)
-}));
+app.MapGet("/bairro/buscar/{uf}/{cidade}", ([FromServices] INeighborhoodService neighborhoodService, string uf, string cidade) => Results.Ok(neighborhoodService.SearchByStateAndCity(uf, cidade)));
 
 app.MapGet("/endereco/buscar/{cep}", ([FromServices] IAddressService addressService, string cep) => Results.Ok(addressService.SearchByCep(cep)));
 
-app.MapGet("/endereco/buscar/cep/{endereco}", ([FromServices] IAddressService addressService, string endereco) => Results.Ok(new ReturnModel<ZipCodeModel>
-{
-    List = addressService.SearchByAddress(endereco)
-}));
+app.MapGet("/endereco/buscar/cep/{endereco}", ([FromServices] IAddressService addressService, string endereco) => Results.Ok(addressService.SearchByAddress(endereco)));
 
 #endregion
 
